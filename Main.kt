652a26ee8d60b8abaeb9ff94354f2c8b5591a408
Adi Trioka <aditrioka@gmail.com>
2025-03-07 16:23:19 +0700

    added phone class

--- Main.kt
+++ Main.kt
@@ -1,3 +1,29 @@
 fun main() {
     println("hello, moto")
+
+    val phone = Phone()
+    phone.topUpCredit(50)
+    phone.call()
+    phone.sms()
+    phone.checkCredit()
+}
+
+class Phone {
+    private var credit = 0
+
+    fun topUpCredit(amount: Int) {
+        credit += amount
+    }
+
+    fun call() {
+        credit -= 10
+    }
+
+    fun sms() {
+        credit -= 1
+    }
+
+    fun checkCredit() {
+        println("Your credit is $credit")
+    }
 }
